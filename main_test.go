59441db7fe8b533516157ea24b7e24ff32a837a8
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-23 11:49:52 -0600

    Prep test file for http testing

--- main_test.go
+++ main_test.go
@@ -1,15 +1,1 @@
 package main
-
-import (
-	"fmt"
-	"testing"
-)
-
-func TestMain(t *testing.M) {
-	got := hello()
-	want := "Hello Learnable"
-
-	if got != want {
-		fmt.Errorf("\nGot: %s\nWant: %s", got, want)
-	}
-}
