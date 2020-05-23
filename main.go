3793c44ff61213006b2b375c9873a270985066fd
Steven Anderson <steven.a.anderson2012@gmail.com>
2020-05-23 11:50:26 -0600

    Set up router through gorilla mux and initial connection for localhost/heroku

--- main.go
+++ main.go
@@ -1,11 +1,43 @@
 package main
 
-import "log"
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	log.Printf(hello())
+	port := GetPort()
+	fmt.Println("Starting Service...")
+
+	fmt.Println("Started...")
+	fmt.Println("Listening on Port: " + port)
+
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/v0/user", userHandler).Methods("GET")
+	http.Handle("/", router)
+
+	err := http.ListenAndServe(port, router)
+
+	if err != nil {
+		panic(err)
+	}
 }
 
-func hello() string {
-	return "Hello Learnable"
+func userHandler(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func GetPort() string {
+	var port = os.Getenv("PORT")
+
+	if port == "" {
+		port = "4747"
+		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+	}
+
+	return ":" + port
 }
